0f0e1e0ecec6afb6e10f1f8a9158f5e380810e4b
Cherif BOUCHELAGHEM <cherif.bouchelaghem@gmail.com>
2019-05-10 18:47:59 +0100

    Landscaper: QUnit2 upgrade

--- src/can-map-setter_test.js
+++ src/can-map-setter_test.js
@@ -3,7 +3,7 @@ require('./can-map-setter');
 require('steal-qunit');
 
 QUnit.module('can/map/setter');
-test('setter testing works', function () {
+QUnit.test('setter testing works', function(assert) {
 	var Contact = can.Map({
 		setBirthday: function (raw) {
 			if (typeof raw === 'number') {
@@ -18,19 +18,19 @@ test('setter testing works', function () {
 			birthday: date.getTime()
 		});
 	// set via constructor
-	equal(contact.birthday.getTime(), date.getTime(), 'set as birthday');
+	assert.equal(contact.birthday.getTime(), date.getTime(), 'set as birthday');
 	// set via attr method
 	date = new Date();
 	contact.attr('birthday', date.getTime());
-	equal(contact.birthday.getTime(), date.getTime(), 'set via attr');
+	assert.equal(contact.birthday.getTime(), date.getTime(), 'set via attr');
 	// set via attr method w/ multiple attrs
 	date = new Date();
 	contact.attr({
 		birthday: date.getTime()
 	});
-	equal(contact.birthday.getTime(), date.getTime(), 'set as birthday');
+	assert.equal(contact.birthday.getTime(), date.getTime(), 'set as birthday');
 });
-test('error binding', 1, function () {
+QUnit.test('error binding', 1, function(assert) {
 	can.Map('School', {
 		setName: function (name, success, error) {
 			if (!name) {
@@ -41,11 +41,11 @@ test('error binding', 1, function () {
 	});
 	var school = new School();
 	school.bind('error', function (ev, attr, error) {
-		equal(error, 'no name', 'error message provided');
+		assert.equal(error, 'no name', 'error message provided');
 	});
 	school.attr('name', '');
 });
-test('asyncronous setting', function () {
+QUnit.test('asyncronous setting', function(assert) {
 	var Meyer = can.Map({
 		setName: function (newVal, success) {
 			setTimeout(function () {
@@ -53,17 +53,17 @@ test('asyncronous setting', function () {
 			}, 1);
 		}
 	});
-	stop();
+	var done = assert.async();
 	var me = new Meyer();
 	me.bind('name', function (ev, newVal) {
-		equal(newVal, 'Justin Meyer');
-		equal(me.attr('name'), 'Justin Meyer');
-		start();
+		assert.equal(newVal, 'Justin Meyer');
+		assert.equal(me.attr('name'), 'Justin Meyer');
+		done();
 	});
 	me.attr('name', 'Justin');
 });
 
-test('setter function values are automatically batched (#815)', function(){
+QUnit.test('setter function values are automatically batched (#815)', function(assert) {
 	var Mapped = can.Map.extend({
 		setFoo: function(newValue){
 			this.attr("zed","ted");
@@ -76,10 +76,10 @@ test('setter function values are automatically batched (#815)', function(){
 
 	map.bind("zed", function(ev){
 		batchNum = ev.batchNum;
-		ok(batchNum, "zed event is batched");
+		assert.ok(batchNum, "zed event is batched");
 	});
 	map.bind("foo", function(ev){
-		equal(batchNum, ev.batchNum, "batchNums are the same");
+		assert.equal(batchNum, ev.batchNum, "batchNums are the same");
 	});
 
 	map.attr("foo","bar");
@@ -89,15 +89,15 @@ test('setter function values are automatically batched (#815)', function(){
 
 //!steal-remove-start
 if (can.dev) {
-	test('setter function warns if a timeout did not happen (#808)', function () {
-		stop();
+	QUnit.test('setter function warns if a timeout did not happen (#808)', function(assert) {
+		var done = assert.async();
 		var oldlog = can.dev.warn;
 		can.dev.warnTimeout = 10;
 		can.dev.warn = function (text) {
-			ok(text, "got a message");
+			assert.ok(text, "got a message");
 			can.batch.stop(true);
 			can.dev.warn = oldlog;
-			start();
+			done();
 		};
 		var Mapped = can.Map.extend({
 			setFoo: function(){}
@@ -108,9 +108,9 @@ if (can.dev) {
 
 	});
 
-	test('setter function does not warn if setter is called back quickly (#808)', function () {
-		stop();
-		expect(1);
+	QUnit.test('setter function does not warn if setter is called back quickly (#808)', function(assert) {
+		var done = assert.async();
+		assert.expect(1);
 		var oldlog = can.dev.warn;
 		can.dev.warnTimeout = 100;
 		var firstMsg = false;
@@ -119,8 +119,8 @@ if (can.dev) {
 			if(!firstMsg) {
 				return;
 			}
-			ok(false, "got a message");
-			start();
+			assert.ok(false, "got a message");
+			done();
 		};
 		var Mapped = can.Map.extend({
 			setFoo: function(newValue, setter){
@@ -133,8 +133,8 @@ if (can.dev) {
 		var map = new Mapped();
 		map.attr("foo", 1);
 		map.bind("foo", function(ev, newVal){
-			equal(newVal, "BAR", "new val set");
-			start();
+			assert.equal(newVal, "BAR", "new val set");
+			done();
 			can.dev.warn = oldlog;
 		});
 
